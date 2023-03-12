c2ee2003bfb0c6f3547c81dbb303120afeccbad3
masakachristopher <masakachristopher@yahoo.com>
2023-03-12 13:45:18 +0300

    import navbar and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Login from "./containers/authentication/Login";
+import Register from "./containers/authentication/Register";
+import { AuthProvider } from "./context/AuthContext";
+import AppRoutes from "./routes";
+import { PrivateRoute } from "./routes/PrivateRoute";
+
 function App() {
   return (
-    <div className="">
-     <p className="">SpidersChallenge Chat App</p>
-    </div>
+    <AuthProvider>
+      <NavBar/>
+      <AppRoutes/>
+    </AuthProvider>
   );
 }
 
