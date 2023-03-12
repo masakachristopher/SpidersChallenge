e99bd0e8502df1e176478503b06e108cf8d14fed
masakachristopher <masakachristopher@yahoo.com>
2023-03-12 13:39:24 +0300

    create constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,21 @@
+export const Pathname = {
+    register: 'signup',
+    login: 'login',
+    chat: 'chat'
+};
+
+export const Strings = {
+  email: "Email",
+  password: "Password",
+  doNotHaveAccount: "Don't have account?",
+  login: "Login",
+  backToLogin: "Back to Login",
+  register: "Register",
+  signIn: "Sign In",
+  username: "Username",
+  signUp: "Sign Up",
+}
+
+export const url = {
+  signup: "signup",
+}
